5e5a82f794799a2410be9d8e9c636850dde3b5fe
Adrian Kupiszewski <adrian.kupiszewski@anixe.pl>
2020-11-25 00:02:04 +0100

    added response type

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,6 +6,7 @@ use std::fmt::{Result as FmtResult, Display, Debug, Formatter};
 use std::str;
 use super::QueryString;
 
+#[derive(Debug)]
 pub struct Request<'buf> {
     path: &'buf str,
     query_string: Option<QueryString<'buf>>,
