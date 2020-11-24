ef0cb20e18b1261316aa59aa25115ff27dd2c72c
Adrian Kupiszewski <adrian.kupiszewski@anixe.pl>
2020-11-24 17:32:44 +0100

    Added tcp listener & new error type

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+use std::io::Read;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use crate::http::Request;
+
 pub struct Server {
     addr: String,
 }
@@ -11,5 +17,25 @@ impl Server {
 
     pub fn run(self) {
         println!("Server is listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection {}", e)
+            }
+        }
     }
 }
